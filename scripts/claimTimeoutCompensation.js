a290a06c90358689e0ac85395ee09e2a8acfc695
vito <zenith@elascript.net>
2025-02-28 14:49:22 +0800

    upgrade stage

--- scripts/claimTimeoutCompensation.js
+++ scripts/claimTimeoutCompensation.js
@@ -2,7 +2,7 @@ const { ethers, network } = require("hardhat");
 const { readConfig } = require("./helper.js");
 async function main() {
   // Transaction ID to claim timeout compensation for
-  const txId = "0x492d988d47f46931e6469fc6bb521e08275ee980a84fea565fc524ca499a47d6";
+  const txId = "0xab5d7f7a19c233fa438619dfe180af6224edc3ed73cbb91aa75b668230a2ac61";
 
   // Get the deployed CompensationManager contract
   const CompensationManager = await ethers.getContractFactory("CompensationManager");
@@ -16,6 +16,10 @@ async function main() {
   const compensationManager = CompensationManager.attach(compensationManagerAddress);
 
   try {
+    let gas = await compensationManager.estimateGas.claimTimeoutCompensation(txId);
+    console.log("gas:", gas);
+    return;
+
     // Call the claimTimeoutCompensation method
     const tx = await compensationManager.connect(signer).claimTimeoutCompensation(
       txId);
